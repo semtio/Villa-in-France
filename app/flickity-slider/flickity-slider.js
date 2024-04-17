abf06abad6483a1cc0d80f22b383cc01a3cace6c
semtio <semik20092@gmail.com>
2024-04-17 20:11:13 +0200

    v.3.4 add slider

---
+++ app/flickity-slider/flickity-slider.js
@@ -0,0 +1,9 @@
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity(elem, {
+    cellAlign: 'left',
+    contain: true,
+    freeScroll: true,
+    wrapAround: true,
+    pageDots: false,
+    prevNextButtons: false,
+});

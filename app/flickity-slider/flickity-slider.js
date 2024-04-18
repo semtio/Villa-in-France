4445b513f6e2b267808cfd78cf83b31471f033f7
semtio <semik20092@gmail.com>
2024-04-18 21:32:25 +0200

    v.4.0 desktop & mobile done

--- app/flickity-slider/flickity-slider.js
+++ app/flickity-slider/flickity-slider.js
@@ -1,13 +1,15 @@
-var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity(elem, {
-    cellAlign: 'left',
-    contain: true,
-    freeScroll: true,
-    wrapAround: true,
-    pageDots: false,
-    prevNextButtons: false,
-    accessibility: false,
-    freeScrollFriction: 0.05,
-    autoPlay: 5000,
-    pauseAutoPlayOnHover: false
-});
+var elem = document.querySelectorAll('.main-carousel');
+for (const i of elem) {
+    var flkty = new Flickity(i, {
+        cellAlign: 'left',
+        contain: true,
+        freeScroll: true,
+        wrapAround: true,
+        pageDots: false,
+        prevNextButtons: false,
+        accessibility: false,
+        freeScrollFriction: 0.07,
+        autoPlay: 5000,
+        pauseAutoPlayOnHover: false
+    });
+}
